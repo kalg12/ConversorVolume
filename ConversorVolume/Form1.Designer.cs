f008a5f7858b668dea436d5b3110c1132dad2977
kalg12 <78965771+kalg12@users.noreply.github.com>
2021-06-30 23:19:35 -0500

    Derechos

--- ConversorVolume/Form1.Designer.cs
+++ ConversorVolume/Form1.Designer.cs
@@ -36,6 +36,7 @@ namespace ConversorVolume
             this.clearButton = new System.Windows.Forms.Button();
             this.ClLabel = new System.Windows.Forms.Label();
             this.litreLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // convertClTitleLabel
@@ -113,12 +114,23 @@ namespace ConversorVolume
             this.litreLabel.TabIndex = 6;
             this.litreLabel.Text = "Litro";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(356, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Programa hecho por kalg12";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
             // volumeConverter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.litreLabel);
             this.Controls.Add(this.ClLabel);
             this.Controls.Add(this.clearButton);
@@ -145,6 +157,7 @@ namespace ConversorVolume
         private System.Windows.Forms.Button clearButton;
         private System.Windows.Forms.Label ClLabel;
         private System.Windows.Forms.Label litreLabel;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ConversorVolume/Form1.cs
+++ ConversorVolume/Form1.cs
@@ -60,5 +60,10 @@ namespace ConversorVolume
         {
             
         }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
